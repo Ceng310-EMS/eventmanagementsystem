50a5f9292b94303fe7ce515dedb7f1f876846749
MNEkin <muhammednurekin@gmail.com>
2014-03-29 18:45:10 +0200

    kullanici girisi

---
+++ testPostgre.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Document</title>
+</head>
+<body>
+		<form action="passControl.php" method="post">
+			<label for="Kullanici_Adi">Kullanıcı Adı</label>
+			<input type="text" name="userName" id="userName" /><br>
+			<label for="Password">Şifre</label>
+			<input type="password" name="pass" id="pass" /><br>
+			<input type="submit" value="Submitt" />
+		</form>
+
+</body>
+</html>
