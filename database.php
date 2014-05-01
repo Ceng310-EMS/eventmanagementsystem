b24c4dc7dfbfc4f98f133eed851288feec678ae6
sly01 <aerkoc01@gmail.com>
2014-05-01 03:31:02 +0300

    Database connection created in specific config file

---
+++ database.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Database Connection</title>
+</head>
+<body>
+	<?php
+		session_start();
+		$connect = pg_connect("host=localhost port=5432 dbname=olayvar user=ceng310 password=eksibir");
+		$_SESSION["connect"] = $connect;
+		if(!$connect)
+		{
+			echo "DB connection issue!!"; die();
+		}
+
+	?>
+</body>
+</html>
